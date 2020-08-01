d555ef5e47a07885446560c27130b51e6b50e063
DCCCXIX <61936752+DCCCXIX@users.noreply.github.com>
2020-08-01 19:23:23 +0300

    Script splitted into two files

--- hemnlp.py
+++ hemnlp.py
@@ -1,45 +1,41 @@
-#!/usr/bin/env python
-
-# from google.colab import files
-
-import os
-import re
-import numpy as np
-import pandas as pd
-import pickle
-import json
-from sklearn.model_selection import train_test_split
-
-# !pip install bpemb
-from bpemb import BPEmb
-
+# !/usr/bin/env python
+# 
+# import os
+# import re
+# import numpy as np
+# import pandas as pd
+# import pickle
+# import json
+# from sklearn.model_selection import train_test_split
+# from bpemb import BPEmb
+# 
 # from google.colab import drive
 # drive.mount('/content/drive')
-
+# 
 # train_data_path = '/content/drive/My Drive/Machine Learning/Hem/train_hem'
 # test_data_path = '/content/drive/My Drive/Machine Learning/Hem/test_hem'
-
+# 
 # #creating dataframes for training and test data to iterate over with .apply() later
 # def read_data(path):
 #   res_data = []
 #   name_list = []
 #   print('Read data...')
-
+# 
 #   if len(os.listdir(path)):
 #     for files in os.listdir(path):
 #       print('>>>',files)
 #       name_list.append(files.split('_')[0])
-
+# 
 #       with open(os.path.join(path,files),'r') as f:
 #         res_data.append(f.read())
 #   return res_data, name_list
-
+# 
 # train_data, train_name_list = read_data(train_data_path)
 # test_data, test_name_list = read_data(test_data_path)
-
+# 
 # train_data = pd.DataFrame(train_data)
 # test_data = pd.DataFrame(test_data)
-
+# 
 # #splitting texts and assigning labels, transforming dataframes into
 # def split_text(text, subtext_length, label):
 #     cursor = 0
@@ -55,21 +51,21 @@ from bpemb import BPEmb
 #             subtexts.append(' '.join(textsplit[cursor:]))
 #             labels.append(label)
 #             break
-
+# 
 #     return subtexts, labels
-
+# 
 # train_hem_texts, train_hem_labels = split_text(train_data[0][0], 300, 1)
 # train_other_texts, train_other_labels = split_text(train_data[0][1], 300, 0)
-
+# 
 # test_hem_texts, test_hem_labels = split_text(test_data[0][0], 300, 1)
 # test_other_texts, test_other_labels = split_text(test_data[0][1], 300, 0)
-
+# 
 # X_train_all = pd.DataFrame(np.append(np.array(train_hem_texts), np.array(train_other_texts)))
 # y_train_all = np.append(np.array(train_hem_labels), np.array(train_other_labels))
-
+# 
 # X_test = pd.DataFrame(np.append(np.array(test_hem_texts), np.array(test_other_texts)))
 # y_test = np.append(np.array(test_hem_labels), np.array(test_other_labels))
-
+# 
 # def PrepareText(data):
 #     tabfeatures = pd.DataFrame()
 #     #Text's total length
@@ -85,12 +81,12 @@ from bpemb import BPEmb
 #     #Amount of upper case letters compared to the text's length
 #     tabfeatures['FULLCAPS_COUNT'] = tabfeatures['uppercase_amount']/tabfeatures['total_length_count']
 #     #Amount of unique words compared to total word count
-
+# 
 #     return tabfeatures
-
+# 
 # X_train_tabfeatures = PrepareText(X_train_all)
 # X_test_tabfeatures = PrepareText(X_test)
-
+# 
 # #cleaning texts - removing special characters, punctuation, html tags, etc.
 # def cleanPunc(text):
 #     no_punctuation = re.sub(r'[?|!|\'|"|#]',r'',text)
@@ -98,12 +94,12 @@ from bpemb import BPEmb
 #     no_punctuation = no_punctuation.strip()
 #     no_punctuation = no_punctuation.replace("\n"," ")
 #     return no_punctuation
-
+# 
 # def cleanHtml(text):
 #     cleanr = re.compile('<.*?>')
 #     no_html = re.sub(cleanr, ' ', str(text))
 #     return no_html
-
+# 
 # def keepAlpha(text):
 #     alpha_sent = ""
 #     for word in text.split():
@@ -112,60 +108,60 @@ from bpemb import BPEmb
 #         alpha_sent += " "
 #     alpha_sent = alpha_sent.strip()
 #     return alpha_sent
-
+# 
 # def cleanText(data):
 #     data[0] = data[0].str.lower()
 #     data[0] = data[0].apply(cleanHtml)
 #     data[0] = data[0].apply(cleanPunc)
 #     data[0] = data[0].apply(keepAlpha)
-
+# 
 #     return data
-
+# 
 # X_train_all = cleanText(X_train_all)
 # X_test = cleanText(X_test)
-
+# 
 # #getting amount of unique words per text
 # X_train_tabfeatures['relative_unique_word_count'] = X_train_all[0].apply(lambda text: (len(set(w for w in text.split()))/len(text.split())))
 # X_test_tabfeatures['relative_unique_word_count'] = X_test[0].apply(lambda text: (len(set(w for w in text.split()))/len(text.split())))
-
+# 
 # def normalize(array):
 #   norm = np.linalg.norm(array)
 #   array = array/norm
 #   return array
-
+# 
 # tabular_features_train = normalize(np.array(X_train_tabfeatures))
 # tabular_features_test = normalize(np.array(X_test_tabfeatures))
-
+# 
 # text_train = np.array(X_train_all[0])
 # text_test = np.array(X_test[0])
-
+# 
 # #checking dataset balance
 # hem = 0
 # other = 0
-
+# 
 # for i in range(len(X_train_all)):
 #     if y_train_all[i] == 0:
 #       other += 1
 #     else:
 #       hem +=1
 # print(f'Hem: {hem}, Other: {other}')
-
+# 
 # bpemb_ru = BPEmb(lang='ru', vs=100000)
 # def encoder(text):
 #     text = bpemb_ru.encode_ids(text)
 #     return text
-
+# 
 # def tokenizer(text):
 #     text = bpemb_ru.encode(text)
 #     return text
-
+# 
 # def embedder(text):
 #     text = bpemb_ru.embed(text)
 #     return text
-
+# 
 # #vectorizing texts with tf/idf
 # from sklearn.feature_extraction.text import TfidfVectorizer
-
+# 
 # vectorizer_word = TfidfVectorizer(
 #                              max_features = 50000,
 #                              strip_accents = 'unicode',
@@ -179,9 +175,9 @@ from bpemb import BPEmb
 #                              min_df = 5,
 #                              max_df = 95
 #                             )
-
+# 
 # vectorizer_word.fit(text_train)
-
+# 
 # vectorizer_char = TfidfVectorizer(
 #                              max_features = 50000,
 #                              strip_accents = 'unicode',
@@ -192,32 +188,32 @@ from bpemb import BPEmb
 #                              smooth_idf = True,
 #                              #sublinear_tf = True
 #                             )
-
+# 
 # vectorizer_char.fit(text_train)
-
+# 
 # #stacking various features into a single sparse matrix for train and test sets
 # from scipy.sparse import hstack
-
+# 
 # train_features_word = vectorizer_word.transform(text_train)
 # test_features_word = vectorizer_word.transform(text_test)
-
+# 
 # train_features_char = vectorizer_char.transform(text_train)
 # test_features_char = vectorizer_char.transform(text_test)
-
+# 
 # train_features = hstack([tabular_features_train, train_features_char, train_features_word]).tocsr()
 # test_features = hstack([tabular_features_test, test_features_char, test_features_word]).tocsr()
-
+# 
 # import lightgbm
-
+# 
 # def Classification(xtrain, ytrain, random_seed, params):
 #     evals_result = {}
 #     train_matrix, valid_matrix, y_train, y_valid = train_test_split(xtrain, ytrain, test_size = 0.2, random_state = random_seed)
-
+# 
 #     d_train = lightgbm.Dataset(train_matrix, label = y_train)
 #     d_valid = lightgbm.Dataset(valid_matrix, label = y_valid)
-
+# 
 #     valid = [d_train, d_valid]
-
+# 
 #     lgbmc = lightgbm.train(params = params,
 #                            train_set = d_train,
 #                            valid_sets = valid,
@@ -226,20 +222,20 @@ from bpemb import BPEmb
 #                            early_stopping_rounds = 2000,
 #                            evals_result = evals_result,
 #                            )
-
+# 
 #     lightgbm.plot_metric(evals_result, figsize=(20,5), metric='xentropy')
-
+# 
 #     return lgbmc
-
+# 
 # #custom evaluation metric to be used for cross validation later as lightgbm doesn't have f1 score 'from the box'
-
+# 
 # from sklearn.metrics import f1_score
-
+# 
 # def lgb_f1_score(y_hat, data):
 #     y_true = data.get_label()
 #     y_hat = np.where(y_hat < 0.5, 0, 1)
 #     return 'f1', f1_score(y_true, y_hat), True
-
+# 
 # #training the model
 # #apparenty not using lambdas or using very low values gives better results
 # params = {
@@ -257,12 +253,12 @@ from bpemb import BPEmb
 #           'lambda_l1': 0.0,
 #           'lambda_l2': 0.0
 #           }
-
+# 
 # model = Classification(train_features, y_train_all, 666, params)
-
+# 
 # #cross validation evaluation
 # cv_dataset = lightgbm.Dataset(train_features, label = y_train_all)
-
+# 
 # cv = lightgbm.cv(
 #     init_model=model,
 #     params=params,
@@ -277,12 +273,12 @@ from bpemb import BPEmb
 #     verbose_eval=50,
 #     shuffle=True
 # )
-
+# 
 # #testing the model
 # from matplotlib import pyplot as plt
 # from sklearn.metrics import confusion_matrix, classification_report
 # import seaborn as sns
-
+# 
 # def test_model(model, x, y):
 #     y_pred = np.round(model.predict(x))
 #     print(classification_report(y_pred, y, labels = [1,0], target_names = ['Hem', 'Other']))
@@ -295,7 +291,7 @@ from bpemb import BPEmb
 #     plt.ylabel('True Class')
 #     plt.xlabel('Predicted Class')
 #     plt.show()
-
+# 
 # test_model(model, test_features, y_test)
-
+# 
 # model.save_model('/content/drive/My Drive/Machine Learning/Hem/super_best_model_200_percent_accuracy_0_loss.txt')
