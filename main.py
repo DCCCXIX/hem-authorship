b6ea06ed0dd75ed73b7d75651d5e3041b7fac74e
Kirill Stolz <stolzkc@gmail.com>
2020-08-01 18:43:41 +0300

    add submit text field and form

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+
+@app.route("/")
+def display_form():
+    return render_template("main_form.html")
+
+
+@app.route("/", methods=["POST"])
+def my_form_post():
+    text = request.form["text"]
+    processed_text = text.upper()
+    return processed_text
